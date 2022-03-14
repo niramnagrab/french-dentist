87a543043d745107c5e94016cf9c07b0b78357ea
niramnagrab <niramnagrab@gmail.com>
2022-03-14 17:32:18 +0800

    Small design improvements

--- src/components/about.js
+++ src/components/about.js
@@ -11,11 +11,14 @@ function About() {
             </div>
 
             <div className="about-text">
-                <h1 className="font-weight-bold ps-3 mt-5 ml-4">{t("part51")}</h1>
-                <div className="aboutsect text-justify font-weight-bolder">
-                    <div className="col-6 text-left">{t("part52")}
+                <h1 className="caretext text-center font-weight-bolder mt-5">{t("part51")}</h1>
+                <div className="hrr">
+                  <span class="line"></span>
+                </div>
+                <div className="aboutsect p-text text-justify">
+                    <div className=" p-text col-6 text-left">{t("part52")}
                     </div>
-                    <div className="col-6 text-left">{t("part53")}
+                    <div className="p-text col-6 text-left">{t("part53")}
                     </div>
                 </div>
             </div>
@@ -51,9 +54,9 @@ function About() {
                 <div><span class='line'></span></div>
                 <div className="sect4">
                     <div className="aboutsect3 mr-5">
-                        <img className="mr-4" src="./homepics/jimao.png" alt="" />
-                        <div>
-                            <h3>{t("part59")}</h3>
+                        <img className="locationimages mr-4" src="./homepics/shanghaimart.png" alt="" />
+                        <div classname="locationtext">
+                            <h3 className="h3-bold">{t("part10")}</h3>
                             <p>
                             {t("part1111")}
                   <br/>
@@ -62,16 +65,28 @@ function About() {
                   {t("part1113")}
                   <br/>
                   {t("part1114")}
+                  <br/>
+                  {t("part1115")}
+                  <br/>
+                  {t("part1116")}
                                 
                                 
                                 </p>
                         </div>
                     </div>
                     <div className="aboutsect3">
-                        <img className="mr-4" src="./homepics/shanghaimart.png" alt="" />
-                        <div>
-                            <h3>{t("part59")}</h3>
-                            <p>{t("part60")}</p>
+                        <img className="mr-4" src="./homepics/jimao.png" alt="" />
+                        <div classname="locationtext">
+                            <h3 className="h3-bold">{t("part14")}</h3>
+                            {t("part1411")}
+                  <br/>
+                  {t("part1412")}
+                  <br/>
+                  {t("part1413")}
+                  <br/>
+                  {t("part1414")}
+                  <br/>
+                  {t("part1415")}
                         </div>
                     </div>
                 </div>
--- src/components/blog.js
+++ src/components/blog.js
@@ -6,6 +6,37 @@ function Blog() {
     const { t } = useTranslation();
     return (
         <div className="blogpage">
+
+            <div className="bloghero">
+                <div className="blogherotext">
+                    <h1 className="h2-bold">The best international dental clinic in Shanghai since 2007</h1>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                </div>
+                <div className="blogheroimage">
+                    <img src="./homepics/blog.jpeg" alt="blog-photo-dentist"  />
+                </div>
+            </div>
+
+            <div className="blogs">
+                <h2 className="h2-bold">More From Our blog</h2>
+                <div className="blogtiles">
+                <div className="blogtile">
+                    <div className="blogtileimage"> <img src="./homepics/blog.jpeg" alt="blog-photo-dentist"  /> </div>
+                    <h3>What happens during a root canal?</h3>
+                    <p>What Happens During a Root Canal? According to the American Association of Endodontists, more than 15 million root canals are performed every year to remove.</p>
+                </div>
+                <div className="blogtile">
+                    <div className="blogtileimage"> <img src="./homepics/blog.jpeg" alt="blog-photo-dentist"  /> </div>
+                    <h3>What happens during a root canal?</h3>
+                    <p>What Happens During a Root Canal? According to the American Association of Endodontists, more than 15 million root canals are performed every year to remove.</p>
+                </div>
+                <div className="blogtile">
+                    <div className="blogtileimage"> <img src="./homepics/blog.jpeg" alt="blog-photo-dentist"  /> </div>
+                    <h3>What happens during a root canal?</h3>
+                    <p>What Happens During a Root Canal? According to the American Association of Endodontists, more than 15 million root canals are performed every year to remove.</p>
+                </div>
+                </div>
+            </div>
            
         </div>
     )
