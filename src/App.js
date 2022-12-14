656a743ceeb1c2cedebdea7c45fb6eabd30f0c11
abdullah motiwala <motiwalaabdullah100@gmail.com>
2022-12-14 21:41:49 +0500

    merge conflicts

--- src/App.js
+++ src/App.js
@@ -12,13 +12,12 @@ import Home from "./components/home";
 import { Container, Navbar, Nav } from "react-bootstrap";
 // import { useHistory } from 'react-router-dom'
 
-import ComprehensiveCare from "./components/services/dental-checkup";
+import DentalCheckUp from "./components/services/dental-checkup";
 import TeethWhitening from "./components/services/teeth-whitening";
-import Orthodontics from "./components/services/teeth-whitening";
+import TeethCleanings from "./components/services/teeth-whitening";
 import Veneers from "./components/services/veneers";
 import Invisalign from "./components/services/invisalign";
 import EmergenciesAndMore from "./components/services/invisalign";
-import BlogDetails from "./components/blog-details";
 
 export default function App() {
   const { t } = useTranslation();
@@ -45,108 +44,22 @@ export default function App() {
   };
   return (
     <>
-        <header className="upperheader">
-          <div className="miniheadertxt">
-            <img className="firstimg" src="./homepics/Icon.png" alt="" />
-            {t("part1")}
-          </div>
-          <div className="socialicons">
-
-
-          <a href="https://instagram.com/frenchdentist?igshid=YmMyMTA2M2Y=" target="_blank">
-          <img className="icon" src="./homepics/Instagram.png" alt="" />
-         </a>  
-
-
-
-         <a href="https://www.linkedin.com/company/french-dentist/" target="_blank">
-         <img className="icon" src="./homepics/linkedin.png" alt="" />
-         </a>  
-
-
-         <a href="#" target="_blank">
-         <img className="icon wechat" src="./homepics/wechat.png" alt="" />
-         </a>  
-            
-          </div>
-        </header>
-
-        <div className="bg-white" >
-          <Navbar collapseOnSelect expand="lg" >
-              <Container className="navbarrr">
-                <Navbar.Brand className="logo-top mainfrlink" href="/">{t("part2")}</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                  <Nav className="me-auto navbarrrr">
-                    <Nav.Link className={`navelem ${pathnameis === '/about' ? 'current' : ''}`}  href="/about">{t("part3")}</Nav.Link>
-                    <Nav.Link className={`navelem ${pathnameis === '/team' ? 'current' : ''}`} href="/team">{t("part4")}</Nav.Link>
-                    <div className="dropdown">
-                      <Nav.Link className={`navelem dropbtn ${pathnameis === '/care' ? 'current' : ''}`}  href="/care">
-                        {t("part5")} 
-                      </Nav.Link>
-                      <div className="dropdown-content">
-                        <a  href="/comprehensive-care">{t("part911")}</a>
-                        <a href="/teeth-whitening">{t("part912")}</a>
-                        <a href="/orthodontics">{t("part913")}</a>
-                        <a href="/dental-implants">{t("part914")}</a>
-                        <a href="/porcelain-veneers">{t("part915")}</a>
-                        <a href="/emergencies-and-more">{t("part916")}</a>
-                      </div>
-                    </div> 
-                    <Nav.Link className={`navelem  ${pathnameis === '/blog' ? 'current' : ''}`} href="/blog">{t("part6")}</Nav.Link>
-                    <Nav.Link className={`navelem  ${pathnameis === '/contactus' ? 'current' : ''}`} href="/contactus">{t("part7")}</Nav.Link>
-                    <Nav.Link>  
-                      <div className="language-select"> 
-                        <LanguageSelect/>
-                      </div>
-                    </Nav.Link>
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
-
-
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Routes>
-            <Route exact path="/" element={<Home />}>
-            </Route>
-            <Route exact path="/about" element={<About />}>
-            </Route>
-            <Route exact path="/team" element={<Team />}>
-            </Route>
-            <Route exact path="/care" element={<Care />}>
-            </Route>
-            <Route exact path="/comprehensive-care" element={<ComprehensiveCare />}>
-            </Route>
-            <Route exact path="/teeth-whitening" element={<TeethWhitening />}>
-            </Route>
-            <Route exact path="/orthodontics" element={<Orthodontics />}>
-            </Route>
-            <Route exact path="/dental-implants" element={<Veneers />}>
-            </Route>
-            <Route exact path="/porcelain-veneers" element={<Invisalign />}>
-            </Route>
-            <Route exact path="/emergencies-and-more" element={<EmergenciesAndMore />}>
-            </Route>
-            <Route exact path="/blog" element={<Blog />}>
-            </Route>
-            <Route exact path="/contactus" element={<Contactus />}>
-            </Route>
-          </Routes>
+      <header className="upperheader">
+        <div className="miniheadertxt">
+          <img className="firstimg" src="./homepics/Icon.png" alt="" />
+          {t("part1")}
         </div>
         <div className="socialicons">
           <img className="icon" src="./homepics/Instagram.png" alt="" />
           <img className="icon" src="./homepics/Twitter.png" alt="" />
-          <img className="icon wechat" src="./homepics/Youtube.png" alt="" />
+          <img className="icon" src="./homepics/Youtube.png" alt="" />
         </div>
       </header>
 
       <div className="bg-white">
         <Navbar collapseOnSelect expand="lg">
           <Container className="navbarrr">
-            <Navbar.Brand className="logo-top mainfrlink" href="/">
+            <Navbar.Brand className="mainfrlink" href="/">
               {t("part2")}
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -222,14 +135,18 @@ export default function App() {
           <Route
             exact
             path="/comprehensive-care"
-            element={<ComprehensiveCare />}
+            element={<DentalCheckUp />}
           ></Route>
           <Route
             exact
             path="/teeth-whitening"
             element={<TeethWhitening />}
           ></Route>
-          <Route exact path="/orthodontics" element={<Orthodontics />}></Route>
+          <Route
+            exact
+            path="/orthodontics"
+            element={<TeethCleanings />}
+          ></Route>
           <Route exact path="/dental-implants" element={<Veneers />}></Route>
           <Route
             exact
@@ -241,12 +158,7 @@ export default function App() {
             path="/emergencies-and-more"
             element={<EmergenciesAndMore />}
           ></Route>
-          <Route exact path="/blog" element={<Blog />}></Route>{" "}
-          <Route
-            exact
-            path="/blog-detail/:slug"
-            element={<BlogDetails />}
-          ></Route>
+          <Route exact path="/blog" element={<Blog />}></Route>
           <Route exact path="/contactus" element={<Contactus />}></Route>
         </Routes>
       </div>
@@ -259,7 +171,7 @@ export default function App() {
           <span className="hrfooterr"></span>
           <div className="footeradd row">
             <div className="col-lg-3">
-              <div className=" address-padding">
+              <div className="text-justify address-padding">
                 <h4>{t("part10")}</h4>
                 {t("part1111")}
                 <br />
@@ -268,38 +180,17 @@ export default function App() {
                 {t("part1113")}
                 <br />
                 {t("part1114")}
-                <br />
-                {t("part1115")}
-                <br />
-                {t("part1116")}
               </div>
             </div>
-            <div className=" col-lg-3 address-padding">
+            <div className="text-justify col-lg-3 address-padding">
               <h4>{t("part12")}</h4>
-              {t("monday")}
+              {t("part13")}
               <br />
-              {t("tuesday")}
-              <br />
-              {t("wednesday")}
-              <br />
-              {t("thursday")}
-              <br />
-              {t("friday")}
-              <br />
-              {t("saturday")}
-              <br />
+              {t("part131")}
             </div>
-            <div className="col-lg-3 address-padding">
+            <div className="text-justify col-lg-3 address-padding">
               <h4>{t("part14")}</h4>
-              {t("part1411")}
-              <br />
-              {t("part1412")}
-              <br />
-              {t("part1413")}
-              <br />
-              {t("part1414")}
-              <br />
-              {t("part1415")}
+              {t("part15")}
             </div>
           </div>
         </footer>
