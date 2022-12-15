036578464982c5e44c61620353d0c9e5bfdef275
codezone12 <codezone64@gmail.com>
2022-12-15 21:26:51 +0500

    mobile responsiveness about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import '../styles/style.css'
+import React from "react";
+import "../styles/style.css";
 import { useTranslation } from "react-i18next";
 import CarouselComponent from "./util-components/Carousel";
 
-
-
 function About() {
-    const { t } = useTranslation();
-    return (
-        <div>
-            {/* <div className="heroabout">
+  const { t } = useTranslation();
+  return (
+    <div>
+      {/* <div className="heroabout">
                 <img className="abouthero" src="./homepics/reception.jpg" alt="" />
             </div> */}
 
-
-<div className="home-hero">
-            <div className="hero-img">
-            <img src="./homepics/aboutus7.png" alt="blog-photo-dentist"  />
-            </div>
-            <div className="about-hero-text">
-                <p className="title-header-left">{t("abouthero")}</p>
-                <p className="subheader-bold-left">{t("abouttext")}</p>
-                <a href="/contact" className="team-btn">{t("aboutbutton")}</a>
-            </div>
+      <div className="home-hero">
+        <div className="hero-img">
+          <img src="./homepics/aboutus7.png" alt="blog-photo-dentist" />
         </div>
+        <div className="about-hero-text">
+          <p className="title-header-left">{t("abouthero")}</p>
+          <p className="subheader-bold-left">{t("abouttext")}</p>
+          <a href="/contact" className="team-btn">
+            {t("aboutbutton")}
+          </a>
+        </div>
+      </div>
 
-            {/* <div className="about-text">
+      {/* <div className="about-text">
                 <h1 className="caretext text-center font-weight-bolder mt-5">{t("part51")}</h1>
                 <div className="hrr">
                   <span class="line-middle"></span>
@@ -38,103 +37,112 @@ function About() {
                 </div>
             </div> */}
 
-<div className="home-team">
-            <div className="left">
-                <div className="main-img">
-                    <img src="./homepics/about10.jpg" alt="blog-photo-dentist"  />
-                </div>
-            </div>
-            <div className="right">
-                <p className="title-header-center">{t("part51")}</p>
-                <div className='hrr'><span class='line-middle'></span></div>
-                <p className="normal-p-text">{t("aboutintro")}</p>
-                <div>
-                <a href="/" className="team-btn">{t("abouttreatments")}</a>
-                </div>
-            </div>
+      <div className="home-team">
+        <div className="left">
+          <div className="main-img">
+            <img src="./homepics/about10.jpg" alt="blog-photo-dentist" />
+          </div>
         </div>
+        <div className="right">
+          <p className="title-header-center">{t("part51")}</p>
+          <div className="hrr">
+            <span class="line-middle"></span>
+          </div>
+          <p className="normal-p-text">{t("aboutintro")}</p>
+          <div className="seeTreatbtn">
+            <a href="/" className="team-btn">
+              {t("abouttreatments")}
+            </a>
+          </div>
+        </div>
+      </div>
 
-            <div className="aboutsect1">
-                <div className="doctor-quotation">
-                <div>{t("part54")}
-                </div>
-                <div className='hrr'><span class='line'></span></div>
-             </div>
-                <div className="doctor-photo">
-                <img className="costa-photo" src="./homepics/costa.jpeg" alt="" />
-                </div>
-                <div>
-                {t("part55")}
-                </div>
-            </div>
+      <div className="aboutsect1">
+        <div className="doctor-quotation">
+          <div>{t("part54")}</div>
+          <div className="hrr">
+            <span class="line"></span>
+          </div>
+        </div>
+        <div className="doctor-photo">
+          <img className="costa-photo" src="./homepics/costa.jpeg" alt="" />
+        </div>
+        <div>{t("part55")}</div>
+      </div>
 
-            {/* <div className="aboutclinicimage">
+      {/* <div className="aboutclinicimage">
                 <img className="imgwidabout" src="./homepics/medicalchair.jpg" alt="" />
             </div> */}
 
-
-{/* <div className="aboutsect2">
+      {/* <div className="aboutsect2">
                 <img className="imgwid" src="./homepics/reception1.jpg" alt="" />
                 <img className="ml-2 imgwid" src="./homepics/reception2.jpg" alt="" />
             </div> */}
-         <div className="carouselContainer">
+      <div className="carouselContainer">
         <CarouselComponent />
         <CarouselComponent orderNumber={2} />
       </div>
 
-            <div className="aboutsect1 patient">
-                <div>
-                    {t("part56")}
-                </div>
-                <div className='hrr'><span className='line'></span></div>
-                <div>{t("part57")}</div>
-            </div>
-
-
-            <div className="pb-5">
-                <div className="title-header-center mt-5 ">{t("part58")}</div>
-                <div><span className='line-middle'></span></div>
-                <div className="sect4">
-                    <div className="aboutsect3 mr-5">
-                        <img className="locationimages mr-4" src="./homepics/shanghaimart.png" alt="" />
-                        <div classname="locationtext">
-                            <h3 className="h3-bold">{t("part10")}</h3>
-                            <p>
-                            {t("part1111")}
-                  <br/>
-                  {t("part1112")}
-                  <br/>
-                  {t("part1113")}
-                  <br/>
-                  {t("part1114")}
-                  <br/>
-                  {t("part1115")}
-                  <br/>
-                  {t("part1116")}
-                                
-                                
-                                </p>
-                        </div>
-                    </div>
-                    <div className="aboutsect3">
-                        <img className="mr-4" src="./homepics/jimao.png" alt="" />
-                        <div classname="locationtext">
-                            <h3 className="h3-bold">{t("part14")}</h3>
-                            {t("part1411")}
-                  <br/>
-                  {t("part1412")}
-                  <br/>
-                  {t("part1413")}
-                  <br/>
-                  {t("part1414")}
-                  <br/>
-                  {t("part1415")}
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className="aboutsect1 patient">
+        <div>{t("part56")}</div>
+        <div className="hrr">
+          <span className="line"></span>
         </div>
-    )
+        <div>{t("part57")}</div>
+      </div>
+
+      <div className="pb-5">
+        <div className="title-header-center mt-5 ">{t("part58")}</div>
+        <div>
+          <span className="line-middle"></span>
+        </div>
+        <div className="sect4">
+          <div className="aboutsect3 mr-5">
+            <img
+              className="locationimages mr-4 aboutSecImg"
+              src="./homepics/shanghaimart.png"
+              alt=""
+            />
+            <div className="locationtext">
+              <h3 className="h3-bold">{t("part10")}</h3>
+              <p>
+                {t("part1111")}
+                <br />
+                {t("part1112")}
+                <br />
+                {t("part1113")}
+                <br />
+                {t("part1114")}
+                <br />
+                {t("part1115")}
+                <br />
+                {t("part1116")}
+              </p>
+            </div>
+          </div>
+          <div className="aboutsect3">
+            <img
+              className="mr-4 aboutSecImg"
+              src="./homepics/jimao.png"
+              alt=""
+            />
+            <div className="locationtext">
+              <h3 className="h3-bold">{t("part14")}</h3>
+              {t("part1411")}
+              <br />
+              {t("part1412")}
+              <br />
+              {t("part1413")}
+              <br />
+              {t("part1414")}
+              <br />
+              {t("part1415")}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
