041603c7d1233d8b9f458ed9d1472bdc58f4b3ba
codezone12 <codezone64@gmail.com>
2022-12-17 00:32:34 +0500

    all screen mob responsiveness and button from carousel hide and hover effect  in blog card added

--- src/components/services/dental-checkup.js
+++ src/components/services/dental-checkup.js
@@ -1,55 +1,56 @@
-import React from 'react'
+import React from "react";
 import { useTranslation } from "react-i18next";
 import Slider from "../slider";
 
 function ComprehensiveCare() {
-    const { t } = useTranslation();
-    return (
-        <div className="ml-5 mr-5 blogpage">
-            <div className="care-hero">
-                <img className="w-100" src="./homepics/blog.png" alt="" />
-                <div className="herotext">
-                    <h2>{t("comprehensive_care")}</h2>
-                    <div>{t("part951")}</div>
-                    <div className="team-btn mt-5">{t("part93")}</div>
-                </div>
-            </div>
-
-            <div>
-                <h2 className="cardheading">{t("part961")}</h2>
-                <div className="blogsect">
-                    <div className="treatment-image"><img src="./homepics/whitening.jpg" alt="" /></div>
-                    <div className="p-2 ml-4 treatment-text">
-                        <div className="mt-3">
-                            <h5>{t("part971")}</h5>
-                            <p>{t("part972")}</p>
-                        </div>
-                        <div className="mt-3">
-                            <h5>{t("part974")}</h5>
-                            <p>{t("part975")}</p>
-                        </div>
-                        <div className="mt-3">
-                            <h5>{t("part976")}</h5>
-                            <p>{t("part977")}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="pt-5">
-                <h2 className="mt-5">{t("part103")} </h2>
-                <div className="blogbtn">
-                    <a  className="team-btn mr-5" >{t("part104")}</a>
-                    <a  className="team-btn mr-5" >{t("part105")}</a>
-                    <a  className="team-btn mr-5" >{t("part106")}</a>
-                    <a  className="team-btn mr-5" >{t("part107")}</a>
-                </div>
-            </div>
-            {/* carousal efect */}
-            <Slider />
-
+  const { t } = useTranslation();
+  return (
+    <div className="ml-5 mr-5 blogpage">
+      <div className="care-hero">
+        <img className="w-100" src="./homepics/blog.png" alt="" />
+        <div className="herotext">
+          <h2>{t("comprehensive_care")}</h2>
+          <div>{t("part951")}</div>
+          <div className="team-btn mt-5">{t("part93")}</div>
         </div>
-    )
+      </div>
+
+      <div>
+        <h2 className="cardheading">{t("part961")}</h2>
+        <div className="blogsect care-blogsect">
+          <div className="treatment-image">
+            <img src="./homepics/whitening.jpg" alt="" />
+          </div>
+          <div className="p-2 ml-4 treatment-text">
+            <div className="mt-3">
+              <h5>{t("part971")}</h5>
+              <p>{t("part972")}</p>
+            </div>
+            <div className="mt-3">
+              <h5>{t("part974")}</h5>
+              <p>{t("part975")}</p>
+            </div>
+            <div className="mt-3">
+              <h5>{t("part976")}</h5>
+              <p>{t("part977")}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-5 other-treatment">
+        <h2 className="mt-5">{t("part103")} </h2>
+        <div className="blogbtn">
+          <a className="team-btn mr-5">{t("part104")}</a>
+          <a className="team-btn mr-5">{t("part105")}</a>
+          <a className="team-btn mr-5">{t("part106")}</a>
+          <a className="team-btn mr-5">{t("part107")}</a>
+        </div>
+      </div>
+      {/* carousal efect */}
+      <Slider />
+    </div>
+  );
 }
 
-export default ComprehensiveCare
+export default ComprehensiveCare;
--- src/components/services/veneers.js
+++ src/components/services/veneers.js
@@ -1,55 +1,54 @@
-import React from 'react'
+import React from "react";
 import { useTranslation } from "react-i18next";
 import Slider from "../slider";
 
 function Veneers() {
-    const { t } = useTranslation();
-    return (
-        <div className="ml-5 mr-5 blogpage">
-            <div className="care-hero">
-                <img className="w-100" src="./homepics/blog.png" alt="" />
-                <div className="herotext">
-                    <h2>{t("part914")}</h2>
-                    <div>{t("part927")}</div>
-                    <div className="team-btn mt-5">{t("part93")}</div>
-                </div>
-            </div>
-
-            <div>
-                <h2 className="mt-5 mb-5 cardheading">{t("part962")}</h2>
-                <div className="blogsect">
-                    <img src="./homepics/implant.jpeg" alt="" />
-                    <div className="p-2 ml-4">
-                        <div className="mt-3">
-                            <h5>{t("part928")}</h5>
-                            <p>{t("part929")}</p>
-                        </div>
-                        <div className="mt-3">
-                            <h5>{t("part930")}</h5>
-                            <p>{t("part931")}</p>
-                        </div>
-                        <div className="mt-3">
-                            <h5>{t("part932")}</h5>
-                            <p>{t("part933")}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="pt-5">
-                <h2 className="mt-5">{t("part103")} </h2>
-                <div className="blogbtn">
-                    <a  className="team-btn mr-5" >{t("part104")}</a>
-                    <a  className="team-btn mr-5" >{t("part105")}</a>
-                    <a  className="team-btn mr-5" >{t("part106")}</a>
-                    <a  className="team-btn mr-5" >{t("part107")}</a>
-                </div>
-            </div>
-            {/* carousal efect */}
-            <Slider />
-
+  const { t } = useTranslation();
+  return (
+    <div className="ml-5 mr-5 blogpage">
+      <div className="care-hero">
+        <img className="w-100" src="./homepics/blog.png" alt="" />
+        <div className="herotext">
+          <h2>{t("part914")}</h2>
+          <div>{t("part927")}</div>
+          <div className="team-btn mt-5">{t("part93")}</div>
         </div>
-    )
+      </div>
+
+      <div>
+        <h2 className="mt-5 mb-5 cardheading">{t("part962")}</h2>
+        <div className="blogsect care-blogsect">
+          <img src="./homepics/implant.jpeg" alt="" />
+          <div className="p-2 ml-4">
+            <div className="mt-3">
+              <h5>{t("part928")}</h5>
+              <p>{t("part929")}</p>
+            </div>
+            <div className="mt-3">
+              <h5>{t("part930")}</h5>
+              <p>{t("part931")}</p>
+            </div>
+            <div className="mt-3">
+              <h5>{t("part932")}</h5>
+              <p>{t("part933")}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-5 other-treatment">
+        <h2 className="mt-5">{t("part103")} </h2>
+        <div className="blogbtn">
+          <a className="team-btn mr-5">{t("part104")}</a>
+          <a className="team-btn mr-5">{t("part105")}</a>
+          <a className="team-btn mr-5">{t("part106")}</a>
+          <a className="team-btn mr-5">{t("part107")}</a>
+        </div>
+      </div>
+      {/* carousal efect */}
+      <Slider />
+    </div>
+  );
 }
 
-export default Veneers
+export default Veneers;
