be0230514bd9fd14de7913def8a2a1b000e1ea29
codezone12 <codezone67@gmail.com>
2022-12-30 02:00:44 +0500

    read more bug resolved

--- src/components/util-components/ReadMore.jsx
+++ src/components/util-components/ReadMore.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 
-function ReadMore({ text, readMore, seeLess, minWords = 500 }) {
+function ReadMore({
+  text,
+  readMore,
+  seeLess,
+  isDrLouis = false,
+  minWords = 500
+}) {
+  let indexOfDotAfter500 = text[0].substring(minWords).indexOf(".");
   const [viewMore, setViewMore] = useState(false);
   return (
     <div>
       <p className="p-text text-justify">
-        {viewMore ? text[0] : text[0].substring(0, minWords)}
+        {viewMore
+          ? text[0]
+          : `${text[0].substring(
+              0,
+              minWords + (isDrLouis ? indexOfDotAfter500 : 0)
+            )}...`}
         <br></br>
         <br />
         {viewMore && text[1] && text[1]}
