7aaa7e0e76d2e691ebd07760d894aa6ee517183e
prateek-pareek <prateekpareekvyas@gmail.com>
2024-08-22 01:40:54 +0530

    fix

--- src/components/slider.js
+++ src/components/slider.js
@@ -13,8 +13,8 @@ function Slider() {
     >
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <div class="testcontainer">
-            <div class="testimonial">
+          <div class="testcontainer d-flex flex-wrap justify-content-cente">
+            <div class="testimonial col-md-4 col-sm-6 col-12">
               <div class="testimonial-content">
                 {t("review.p1")}
               </div>
@@ -26,58 +26,35 @@ function Slider() {
                 <span class="star">★</span>
               </div>
               <div class="testimonial-author">
-                <img src="./photos/review/1.png" alt="Maria"/>
+                <img src="./photos/review/1.png" alt="Maria" />
                 <div>
                   <span style={{ fontWeight: 'bold', color: 'white' }}> {t("review.a1")}</span>
                   <br />
                   <span style={{ color: 'lightgrey' }}>29 July 2024</span>
                 </div>
-
               </div>
-
             </div>
-
-
-            <div class="testimonial">
-
+            <div class="testimonial col-md-4 col-sm-6 col-12">
               <div class="testimonial-content">
-
                 {t("review.p2")}
-
               </div>
-
               <div class="stars">
-
                 <span class="star">★</span>
-
                 <span class="star">★</span>
-
                 <span class="star">★</span>
-
                 <span class="star">★</span>
-
                 <span class="star">★</span>
-
               </div>
-
               <div class="testimonial-author">
-
                 <img src="./photos/review/2.png" alt="Amy" />
-
-                
                 <div>
                   <span style={{ fontWeight: 'bold', color: 'white' }}> {t("review.a2")}</span>
                   <br />
                   <span style={{ color: 'lightgrey' }}>29 July 2024</span>
                 </div>
-
-              
-         </div>
+              </div>
             </div>
-
-
-            <div class="testimonial">
-
+            <div class="testimonial col-md-4 col-sm-6 col-12">
               <div class="testimonial-content">
                 {t("review.p3")}
               </div>
@@ -95,15 +72,13 @@ function Slider() {
                   <br />
                   <span style={{ color: 'lightgrey' }}>29 July 2024</span>
                 </div>
-
               </div>
-
             </div>
           </div>
         </div>
         <div className="carousel-item">
-          <div class="testcontainer">
-            <div class="testimonial">
+          <div class="testcontainer d-md-flex flex-md-row">
+            <div class="testimonial col-md-4 col-12">
               <div class="testimonial-content">
                 {t("review.p4")}
               </div>
@@ -123,31 +98,12 @@ function Slider() {
                 </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
 
-      <a
-        className="carousel-control-prev"
-        href="#carouselExampleControls"
-        role="button"
-        data-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">{t("previous")}</span>
-      </a>
-      <a
-        className="carousel-control-next"
-        href="#carouselExampleControls"
-        role="button"
-        data-slide="next"
-      >
-        <span
-          className="carousel-control-next-icon prebtn"
-          aria-hidden="true"
-        ></span>
-        <span className="sr-only">{t("next")}</span>
-      </a>
+    
     </div>
   );
 }
