4d1641200d21f0fd01c1f36092473e35fedba97c
prateek-pareek <prateekpareekvyas@gmail.com>
2024-08-24 23:24:25 +0530

    ficx

--- src/components/slider.js
+++ src/components/slider.js
@@ -10,22 +10,26 @@ function Slider() {
       data-wrap={true}
       data-ride={false}
       data-interval={false}
-     
+
     >
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <div class="testcontainer d-flex flex-wrap justify-content-cente">
-            <div class="testimonial col-md-4 col-sm-6 col-12">
-              <div class="testimonial-content">
-                {t("review.p1")}
-              </div>
-              <div class="stars">
-                <span class="star">★</span>
-                <span class="star">★</span>
-                <span class="star">★</span>
-                <span class="star">★</span>
-                <span class="star">★</span>
-              </div>
+          <div class="testcontainer">
+            <div class="testimonial mr-1">
+             
+
+
+                <div class="testimonial-content">
+                  {t("review.p1")}
+                </div>
+                <div class="stars">
+                  <span class="star">★</span>
+                  <span class="star">★</span>
+                  <span class="star">★</span>
+                  <span class="star">★</span>
+                  <span class="star">★</span>
+                </div>
+              
               <div class="testimonial-author">
                 <img src="./photos/review/1.png" alt="Maria" />
                 <div>
@@ -35,7 +39,8 @@ function Slider() {
                 </div>
               </div>
             </div>
-            <div class="testimonial col-md-4 col-sm-6 col-12">
+            <div class="testimonial mr-1">
+           
               <div class="testimonial-content">
                 {t("review.p2")}
               </div>
@@ -46,6 +51,7 @@ function Slider() {
                 <span class="star">★</span>
                 <span class="star">★</span>
               </div>
+              
               <div class="testimonial-author">
                 <img src="./photos/review/2.png" alt="Amy" />
                 <div>
@@ -55,7 +61,7 @@ function Slider() {
                 </div>
               </div>
             </div>
-            <div class="testimonial col-md-4 col-sm-6 col-12">
+            <div class="testimonial mr-1">
               <div class="testimonial-content">
                 {t("review.p3")}
               </div>
@@ -77,34 +83,10 @@ function Slider() {
             </div>
           </div>
         </div>
-        <div className="carousel-item">
-          <div class="testcontainer d-md-flex flex-md-row">
-            <div class="testimonial col-md-4 col-12">
-              <div class="testimonial-content">
-                {t("review.p4")}
-              </div>
-              <div class="stars">
-                <span class="star">★</span>
-                <span class="star">★</span>
-                <span class="star">★</span>
-                <span class="star">★</span>
-                <span class="star">★</span>
-              </div>
-              <div class="testimonial-author">
-                <img src="./photos/review/4.png" alt="Maria" />
-                <div>
-                  <span style={{ fontWeight: 'bold', color: 'white' }}> {t("review.a4")}</span>
-                  <br />
-                  <span style={{ color: 'lightgrey' }}>29 July 2024</span>
-                </div>
-              </div>
-            </div>
 
-          </div>
-        </div>
       </div>
 
-    
+
     </div>
   );
 }
